7150dc22e55b1b6b9573a39a855ece9298e97edb
Miguel <miguelapb35@gmail.com>
2018-12-17 20:50:38 -0500

    Add files via upload
    three.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation()
+
+$('[data-open-details]').click(function (e) {
+    e.preventDefault();
+    $(this).next().toggleClass('is-active');
+    $(this).toggleClass('is-active');
+  });
+  
+  
